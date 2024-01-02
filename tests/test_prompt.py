ac9d895d719f8f45b4ffda7271dc5e5b3de84a07
MuhammadNizamani <ishaque.niz786@gmail.com>
2024-01-02 11:35:55 +0500

    RM: removed the test file for prompt

--- tests/test_prompt.py
+++
@@ -1,8 +0,0 @@
-# from src import prompt
-
-
-# def test_pdf_to_text():
-#     # Call the function to convert PDF to text
-#     result_text = prompt.prompt()
-#     # Assert that the result is not an empty string
-#     assert result_text.strip()  # This will raise an error if the string is empty

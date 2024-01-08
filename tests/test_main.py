2df80ae4a74caebf07f1a9f8efd42e9f478f161f
Salman-18 <salmannizamani205@gmail.com>
2024-01-08 11:18:52 +0500

    change in workflow331

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,9 +9,5 @@ def test_pdf_to_text():
     # Call the function to convert PDF to text
     result_text = main.pdf_to_text(pdf_path)
 
-    
-    
-    
-    
     # Assert that the result is not an empty string
     assert result_text.strip()  # This will raise an error if the string is empty
